9b5596dbaf76d2d5601ca8891427efc1b3a329ce
Debaditya Mohankudo <deb.adityamohankudo@gmail.com>
2019-09-30 08:21:19 +0530

    Update simple-recursions.py

--- simple-recursions.py
+++ simple-recursions.py
@@ -6,3 +6,10 @@ def is_pal(str_i): #palindrome
             return is_pal(str_i[1:-1])
         else:
             return False
+
+def fib(n): # not very efficient -> too many duplicate calls with increase in input
+    """asssume n> 1"""
+    if n==0 or n==1:
+        return 1
+    else:
+        return  fib(n-1) + fib(n-2)
