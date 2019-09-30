2dbcdcf7c599d8b9e29157e96eb4aeb0096c0a74
Debaditya Mohankudo <deb.adityamohankudo@gmail.com>
2019-09-30 08:56:30 +0530

    Update simple-recursions.py

--- simple-recursions.py
+++ simple-recursions.py
@@ -6,23 +6,26 @@ def is_pal(str_i): #palindrome
             return is_pal(str_i[1:-1])
         else:
             return False
+        
 ##################################################################################
+
 def fib(n): # not very efficient -> too many duplicate calls with increase in input
     """asssume n> 1"""
     if n==0 or n==1:
         return 1
     else:
         return  fib(n-1) + fib(n-2)
+    
 ##################################################################################
 
 from collections import defaultdict
-def fib(n, memoize=None): # more efficient than the earlier version as it stores the incremental results and reuses
+def fib(n, memoize=None):
     if memoize is None:
         memoize = defaultdict(int)
-        memoize[0] = 1
-        memoize[1] = 1
+        
     if n==0 or n==1:
-        return 1
+        memoize[n] = 1
+        return memoize[n]
     else:
         
         if memoize[n] != 0:
@@ -30,7 +33,9 @@ def fib(n, memoize=None): # more efficient than the earlier version as it stores
         else:
             memoize[n-1], memoize[n-2] = fib(n-1, memoize), fib(n-2, memoize)
             return memoize[n-1] + memoize[n-2]
+        
 ################################################################################
+
 # better than recursion fibonacci ??
 def fib(n):
     x, y = 0, 1
