4368b7b6db19f3c95bb24982ea04c091ca3e741d
Debaditya Mohankudo <deb.adityamohankudo@gmail.com>
2019-09-30 07:50:42 +0530

    Update all_combinations.py

--- all_combinations.py
+++ all_combinations.py
@@ -22,5 +22,18 @@ def all_combinations(str_S):
         return str_S
     return [char + combinations for i, char in enumerate(str_S) for combinations in all_combinations(remainingChar(str_S, i))]
 
+def all_combinations(str_S, word='', res_L=None):
+    if res_L is None:
+        res_L = []
+
+    if len(str_S) == 1:#base case
+        res_L.append(word + str_S)
+        return
+
+    else: #recursive case
+        for i, char in enumerate(str_S):
+            all_combinations(remainingChar(str_S, i), word + char, res_L)
+        return res_L
+
 all_combinations('abc')
 >>> ['abc', 'acb', 'bac', 'bca', 'cab', 'cba']
