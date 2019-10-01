ff843e0f31672677cb5619ab92bf8309224fadd2
Debaditya Mohankudo <deb.adityamohankudo@gmail.com>
2019-10-01 15:06:51 +0530

    Update simple-recursions.py

--- simple-recursions.py
+++ simple-recursions.py
@@ -7,7 +7,9 @@ def is_pal(str_S):
             return is_pal(str_S[1:-1])
         else:
             return False
+        
 ###########################################
+
 def dec_to_binary(number):
     if number < 2:
         return str(number)
@@ -15,4 +17,14 @@ def dec_to_binary(number):
         reminder = number % 2
         remaining = number // 2
         return dec_to_binary(remaining) + str(reminder)
+    
 ############################################################
+
+def find_gcd(num_N1, num_N2):
+    if num_N1 == 0:
+        return num_N2
+    elif num_N2 == 0:
+        return num_N1
+    elif num_N1 == 1 or num_N2 == 1:
+        return 1
+    return find_gcd(min(num_N1, num_N2), max(num_N1, num_N2) % min(num_N1, num_N2))
