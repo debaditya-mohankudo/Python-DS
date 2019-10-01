af3341f2b68ba089152645cadf3c4a9e6f3b714a
Debaditya Mohankudo <deb.adityamohankudo@gmail.com>
2019-10-01 14:30:25 +0530

    Update and rename simple-recursions.py to fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,12 +1,3 @@
-def is_pal(str_i): #palindrome
-    if len(str_i) == 1:
-        return True
-    else:
-        if str_i[0] == str_i[-1]:
-            return is_pal(str_i[1:-1])
-        else:
-            return False
-        
 ##################################################################################
 
 def fib(n): # not very efficient -> too many duplicate calls with increase in input
